8e24687ec9e7d70f5b0877e0179cf9b858e3dd84
vic3mjk <vicente.alons.sm@gmail.com>
2026-04-06 14:27:49 -0400

    Update ejercicio1.py

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -1,22 +1,12 @@
-"""
-Ejercicio 1: Dado un número entero positivo N, retornar una lista con los números desde 1 hasta N.
-
-Debe implementar dos funciones:
-1. Una usando iteración (for o while)
-2. Una usando recursividad
-"""
-
 def contar_ciclo(n):
     num = []
     for i in range(1, n + 1):
          num.append(i)
     return num
-print(contar_ciclo(10))nbv
-asasd
+print(contar_ciclo(10))
 
 def contar_recursivo(n):
-    """
-    Retorna una lista con los números desde 1 hasta n usando recursividad.
-    """
-    # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    if n <= 0:
+        return []
+    return contar_recursivo(n - 1) + [n]
+print(contar_recursivo(10))
