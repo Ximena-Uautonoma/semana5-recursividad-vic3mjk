0fe802966d7ffe278d2339f79da4aa921c636d80
vic3mjk <vicente.alons.sm@gmail.com>
2026-04-08 13:45:41 -0400

    Factorial ciclo

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,7 +6,11 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
+    multi = 1
+    for i in range(1, n + 1):
+        multi = multi * i
+    return multi
+print(factorial_ciclo(int(input("Ingrese un número: "))))    
 
 
 def factorial_recursivo(n):
