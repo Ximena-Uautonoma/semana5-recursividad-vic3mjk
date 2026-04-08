c1d5739a339486c484f81dc0ad1d8b98ba643812
vic3mjk <vicente.alons.sm@gmail.com>
2026-04-08 14:02:31 -0400

    Pares ciclos y factorial

--- Semana5/ejercicio4.py
+++ Semana5/ejercicio4.py
@@ -4,8 +4,18 @@ Dado un número entero positivo N, contar cuántos números pares existen entre 
 """
 
 def contar_pares_ciclo(n):
-    pass
-
+    lista = []
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            lista.append(i)
+    return f"Existem {len(lista)} pares los cuales son {lista}"
+print(contar_pares_ciclo(int(input("Ingrese un número positivo: "))))
 
 def contar_pares_recursivo(n):
-    pass
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + contar_pares_recursivo(n - 1)
+    else:
+        return contar_pares_recursivo(n - 1)
+print(contar_pares_recursivo(int(input("Ingrese un número positivo: "))))
