527a26e8de5805028fc8553d0378ecc5512ffdab
vic3mjk <vicente.alons.sm@gmail.com>
2026-04-08 14:13:06 -0400

    Potencia ciclo y recursiva

--- Semana5/ejercicio5.py
+++ Semana5/ejercicio5.py
@@ -4,8 +4,15 @@ Calcular la potencia de una base elevada a un exponente entero positivo.
 """
 
 def potencia_ciclo(base, exponente):
-    pass
-
+    resultado = 1
+    for i in range(1, exponente + 1):
+        resultado = base * resultado
+    return resultado
+print(potencia_ciclo(int(input("ingrese la base: ")),int(input("Ingrese el exponente: "))))
 
 def potencia_recursiva(base, exponente):
-    pass
+    if exponente == 0:
+        return 1
+    else:
+        return base * potencia_recursiva(base, exponente-1)
+print(potencia_recursiva(int(input("ingrese la base: ")),int(input("Ingrese el exponente: "))))
